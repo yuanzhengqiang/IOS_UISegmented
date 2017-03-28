cc1befacf949208b6f053a386a5af922e37be17a
yuanzhengqiang <273816488@qq.com>
2017-03-28 08:55:54 +0800

    Initial Commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by 原政强 on 17/3/28.
+//  Copyright © 2017年 yzq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
